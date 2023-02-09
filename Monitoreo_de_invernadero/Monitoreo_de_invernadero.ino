a76338f5bfc43fbba75a6a9ff26876f5672c0be8
Antonio Carbajal <aamorenoc@ipn.mx>
2023-02-09 11:12:26 -0600

    Esqueleto del codigo principal

---
+++ Monitoreo_de_invernadero/Monitoreo_de_invernadero.ino
@@ -0,0 +1,35 @@
+/*
+ * Centro de Innovación e Integración de Tecnologías Avanzadas
+ * Unidad Veracruz
+ * Departamento de electrónica
+ * 
+ * Elaborado por:
+ *  Antonio del Angel Moreno Carbajal     Jefe de departamento
+ *  Maria Isabel Cruz Solis               Residente del departamento
+ * 
+ * Fecha:
+ *  09/02/2023
+ * 
+ * Descripción:
+ *  El siguiente sistema realiza el monitoreo de un invernadero rustico con componentes de bajo costo.
+ *  
+ * Componentes:
+ *  Tarjeta de desarroollo ESP32
+ *  Sensor de humedad
+ *  Sensor de radiación UV
+ * 
+ * Tipo de conexión:
+ *  Inalambrica (Wi-FI)
+ */
+
+void setup() {
+  pinMode(,OUTPUT);
+  pinMode(,OUTPUT);
+  pinMode(,INPUT);
+  Serial.begin(115200);
+
+}
+
+void loop(){
+  
+}
